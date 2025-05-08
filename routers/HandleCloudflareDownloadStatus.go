3980d9cc410e1d74f7b90a8ff84d7e6f0abe2ce6
Aiden Appleby <aiden.appleby3@gmail.com>
2025-05-08 14:26:36 -0400

    Update HandleCloudflareDownloadStatus.go

--- routers/HandleCloudflareDownloadStatus.go
+++ routers/HandleCloudflareDownloadStatus.go
@@ -1,12 +1,9 @@
 package routers
 
 import (
-	"fmt"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/hillview.tv/videoAPI/env"
 )
 
 func HandleCloudflareDownloadStatus(w http.ResponseWriter, r *http.Request) {
@@ -19,33 +16,32 @@ func HandleCloudflareDownloadStatus(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	cloudflareAccountID := env.CloudflareUID
-	cloudflareAPIToken := env.CloudflareToken
-	endpoint := fmt.Sprintf("https://api.cloudflare.com/client/v4/accounts/%s/stream/%s", cloudflareAccountID, id)
+	// cloudflareAccountID := env.CloudflareUID
+	// cloudflareAPIToken := env.CloudflareToken
+	// endpoint := fmt.Sprintf("https://api.cloudflare.com/client/v4/accounts/%s/stream/%s", cloudflareAccountID, id)
 
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", endpoint, nil)
-	if err != nil {
-		http.Error(w, "Failed to create request: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
+	// client := &http.Client{}
+	// req, err := http.NewRequest("GET", endpoint, nil)
+	// if err != nil {
+	// 	http.Error(w, "Failed to create request: "+err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
 
-	req.Header.Set("Authorization", "Bearer "+cloudflareAPIToken)
+	// req.Header.Set("Authorization", "Bearer "+cloudflareAPIToken)
 
-	resp, err := client.Do(req)
-	if err != nil {
-		http.Error(w, "Failed to fetch video status: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer resp.Body.Close()
+	// resp, err := client.Do(req)
+	// if err != nil {
+	// 	http.Error(w, "Failed to fetch video status: "+err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+	// defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		http.Error(w, "Failed to read response body: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
+	// body, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	http.Error(w, "Failed to read response body: "+err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
 
 	// Forward the response from Cloudflare
-	w.WriteHeader(resp.StatusCode)
-	w.Write(body)
+	w.WriteHeader(http.StatusNotImplemented)
 }
